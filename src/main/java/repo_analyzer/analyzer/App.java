d9bb4ea04db23d44c9d155c4c352b5e8f6b193da
Mateus Luiz <mateuslfreitasb@gmail.com>
2019-10-24 17:49:47 -0300

    Generate erlang visitors

--- src/main/java/repo_analyzer/analyzer/App.java
+++ src/main/java/repo_analyzer/analyzer/App.java
@@ -1,9 +1,19 @@
 package repo_analyzer.analyzer;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.List;
 
+import org.antlr.v4.runtime.ANTLRInputStream;
+import org.antlr.v4.runtime.CommonTokenStream;
+
 import repo_analyzer.analyzer.services.S3Service;
+import repo_analyzer.analyzer.visitors.ErlangBaseVisitor;
+import repo_analyzer.analyzer.visitors.ErlangLexer;
+import repo_analyzer.analyzer.visitors.ErlangParser;
 
 /**
  * Hello world!
@@ -11,6 +21,7 @@ import repo_analyzer.analyzer.services.S3Service;
  */
 public class App {
     public static void main( String[] args ) {
+    	/*
     	S3Service s3Service = new S3Service();
     	List<String> fileNames = s3Service.listBucketFiles();
     	
@@ -21,5 +32,21 @@ public class App {
 		}
     	
     	System.out.println(fileNames.get(0));
+    	*/
+    	
+    	/*
+    	InputStream inputstream;
+		try {
+			inputstream = new FileInputStream("tmp/helloword.erl");
+			ErlangLexer lexer = new ErlangLexer(new ANTLRInputStream(inputstream));
+			ErlangParser parser = new ErlangParser(new CommonTokenStream(lexer));
+		    new ErlangBaseVisitor().visitForms(parser.forms());
+		    
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		*/
+
     }
 }
